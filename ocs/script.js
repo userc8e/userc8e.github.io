eef9d097b0bd8ffc5c29a2379a97c6eef10d6aae
userc8e <caitlynquack@gmail.com>
2023-08-31 22:54:04 -0400

    js things i think idk i dont rmbr

--- ocs/script.js
+++ ocs/script.js
@@ -1,35 +1,23 @@
-const mochaBtn = document.querySelector(".mochaBtn");
-const mizukiBtn = document.querySelector(".mizukiBtn");
-const karinBtn = document.querySelector(".karinBtn");
-const shouyaBtn = document.querySelector(".shouyaBtn");
-
 const infoFolder = document.querySelector(".infoFolder");
-const dialog = document.querySelector(".dialog");
+const info = document.querySelector(".info");
 
-
-/* how the button should work in theory?????
-    when you click on a charaTab button,
-    it changes the background color of infoFolder to match the color of that button
-    and then it changes all the info inside that div
-    including h2, paragraphs, and images maybe idk
-    but! how does this work!! idk!!!!!!!
-
-    erm do i need to use classes bc idk how those work tee bee aych
-*/
-
+const mochaBtn = document.querySelector(".mochaBtn");
 mochaBtn.addEventListener("click", () => {
     infoFolder.style.backgroundColor = "#c0bff4";
-    dialog.show();
+    info.style.visibility = "visible";
 });
 
+const mizukiBtn = document.querySelector(".mizukiBtn");
 mizukiBtn.addEventListener("click", () => {
     infoFolder.style.backgroundColor = "#ffafbd";
 });
 
+const karinBtn = document.querySelector(".karinBtn");
 karinBtn.addEventListener("click", () => {
     infoFolder.style.backgroundColor = "#accdc9";
 });
 
+const shouyaBtn = document.querySelector(".shouyaBtn");
 shouyaBtn.addEventListener("click", () => {
     infoFolder.style.backgroundColor = "#bdbdca";
 });
